f323421813b15fa095c7199a7a4a2854e27ff5f6
Corrochano Paniagua <ivan.corrochano@ineco.com>
2022-09-06 10:05:34 +0200

    Try to read S3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,32 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+'''
+Código para la creación de una aplicación para la realización de estudios
+operacionales en streamlit
+'''
+
 import streamlit as st
+import s3fs
+import os
+import pandas as pd
 
-"""
-# Welcome to Streamlit!
+# Read secrets
+AWS_ACCESS_KEY_ID = st.secrets['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = st.secrets['AWS_SECRET_ACCESS_KEY']
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Create connection object
+fs = s3fs.S3FileSystem(anon=False)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Read luces.csv
+luces = pd.read_csv(
+    "s3://proyectos-internos/Estudios_Operacionales/Luces.csv",
+    sep=';',
+    storage_options={
+        "key": AWS_ACCESS_KEY_ID,
+        "secret": AWS_SECRET_ACCESS_KEY
+    }
+)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# An airport is selected
+aeropuertos = luces['AEROPUERTO'].drop_duplicates()
+aeropuerto = st.selectbox('Seleccione un aeropuerto', aeropuertos)
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
-    
-    
-    option = st.selectbox(
-    'Selecciona un Aeropuerto',
-     ['LEMD', 'LEMG'])
+st.write('Has seleccionado', aeropuerto)
